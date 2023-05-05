94261ac1d4eceb3f79730af632169d50896c69ca
William Deyns <DeynsPC@Outlook.com>
2023-05-05 19:36:33 -0300

    JALA DINO
    Grupo 2, turma 9 do modulo 02

--- dino_runner/components/powerups/shield.py
+++ dino_runner/components/powerups/shield.py
@@ -4,6 +4,4 @@ from dino_runner.components.powerups.power_up import PowerUp
 
 class Shield(PowerUp):
     def __init__(self):
-        self.image = SHIELD
-        self.type = SHIELD_TYPE
-        super().__init__(self.image, self.type)
+        super().__init__(SHIELD,SHIELD_TYPE )
--- dino_runner/utils/text_utils.py
+++ dino_runner/utils/text_utils.py
@@ -1,10 +1,10 @@
 import pygame
 
-from dino_runner.utils.constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from dino_runner.utils.constants import SCREEN_HEIGHT, SCREEN_WIDTH, FONT
 
 FONT_COLOR = (0, 0, 0)
 FONT_SIZE = 22
-FONT_STYLE = "freesansbold.ttf"
+FONT_STYLE = FONT
 
 
 def draw_message_component(
@@ -16,7 +16,7 @@ def draw_message_component(
     pos_x_center=SCREEN_WIDTH // 2
 ):
     font = pygame.font.Font(FONT_STYLE, font_size)
-    text = font.render(message, True, font_color)
+    text = font.render(message, False, font_color)
     text_rect = text.get_rect()
     text_rect.center = (pos_x_center, pos_y_center)
     screen.blit(text, text_rect)
